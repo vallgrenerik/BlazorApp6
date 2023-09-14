5ccc028110d653f48ba0ca07da03a98b4d531855
Erik Vallgren <erik.vallgren@umu.se>
2023-09-14 08:49:05 +0200

    add all the things

---
+++ BlazorApp6/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp6;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options)
+	{
+	}
+}
+public class AppUser : IdentityUser
+{
+	// Add customizations here later
+}
--- BlazorApp6/Models/WeatherForecast.cs
+++ BlazorApp6/Models/WeatherForecast.cs
@@ -1,12 +1,6 @@
 namespace BlazorApp6;
 
-public class WeatherForecast
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
-	public DateOnly Date { get; set; }
-
-	public int TemperatureC { get; set; }
-
-	public string? Summary { get; set; }
-
 	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
